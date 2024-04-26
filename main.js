51d3346fc5db2ddab2139cef378897357aac9c85
AbrahamOliden <abrahamoliden@protonmail.com>
2024-04-25 18:59:22 -0600

    improved code swapping while loop to for...of loop

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 const keyList = document.querySelectorAll('.tecla');
 
-let index = 0;
-
-while (index < 9) {
-    const button = keyList[index];
+for (const button of keyList) {
     const sound = button.classList[1];
 
     button.onclick = () => {
         playSound(`#sonido_${sound}`);
     };
-    index++;
 };
 
 function playSound(id) {
     document.querySelector(id).play();
+    console.log(id);
 };
