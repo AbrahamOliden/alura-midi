d69f06489463c795350b863b296e537d9e1e0587
AbrahamOliden <abrahamoliden@protonmail.com>
2024-04-25 18:35:53 -0600

    function playSound implemented on each button with while loop

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 const keyList = document.querySelectorAll('.tecla');
+
+let index = 0;
+
+while (index < 9) {
+    const button = keyList[index];
+    const sound = button.classList[1];
+
+    button.onclick = () => {
+        playSound(`#sonido_${sound}`);
+    };
+    index++;
+};
+
+function playSound(id) {
+    document.querySelector(id).play();
+};
