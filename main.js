dc59228d4c773316c668217dfb3e45fa713f23dc
AbrahamOliden <abrahamoliden@protonmail.com>
2024-04-25 21:48:04 -0600

    onkeydown and onkeyup events for buttons

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-const keyList = document.querySelectorAll('.tecla');
+const buttonList = document.querySelectorAll('.tecla');
 
-for (const button of keyList) {
+for (const button of buttonList) {
     const sound = button.classList[1];
 
     button.onclick = () => {
         playSound(`#sonido_${sound}`);
     };
+
+    button.onkeydown = (event) => {
+        if (event.code === 'Space' || event.code === 'Enter') {
+            button.classList.add('activa');
+        };
+    };
+
+    button.onkeyup = () => button.classList.remove('activa');
 };
 
 function playSound(id) {
     document.querySelector(id).play();
-    console.log(id);
 };
